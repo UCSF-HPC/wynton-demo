29ac124ca3df21fbc1e273b612b208f82eae6035
hb <henrik.bengtsson@gmail.com>
2024-11-13 14:51:53 -0800

    tidy up

--- fuse-tmpdir.sh
+++ fuse-tmpdir.sh
@@ -1,4 +1,7 @@
 #!/bin/env bash
+## -------------------------------------------------------------------------
+## SGE directives
+## -------------------------------------------------------------------------
 #$ -S /bin/bash     # the shell language when run via the job scheduler [IMPORTANT]
 #$ -notify          # tell SGE to shut down job nicely [IMPORTANT]
 #$ -cwd             # use current working directory
@@ -7,33 +10,50 @@
 #$ -l scratch=1G    # request 1 GiB of /scratch space
 
 
+## -------------------------------------------------------------------------
+## Script prologue
+## -------------------------------------------------------------------------
 ## Set up size-limited TMPDIR folder (per -l scratch=<size>, if used)
+## The TMPDIR folder is automatically removed when the script exits
 module load CBI wynton-tools
-eval "$(wynton utils fuse-tmpdir --apply)"
+eval "$(wynton utils --apply fuse-tmpdir)"
 
+
+## -------------------------------------------------------------------------
+## Main script
+## -------------------------------------------------------------------------
+echo "* EXAMPLE: Size-limited TMPDIR folder example ..."
 
 ## Using the size-limited TMPDIR folder
 echo "TMPDIR: ${TMPDIR}"
-df -h "${TMPDIR}"
-stat "${TMPDIR}"
 
-echo "Using size-limited TMPDIR"
+echo "Available size and current usage of the TMPDIR folder:"
+df -h "${TMPDIR}"
+
+echo "Creating a tempory directory:"
 td=$(mktemp -d)
-echo "td=${td}"
+
+echo "Writing the current date to a 'now' file in the temporary directory:"
 date > "${td}"/now
 cat "${td}"/now
 
-
-echo "Creating a 0.5 GiB temporary file"
+echo "Creating a 0.5 GiB file in the temporary directory:"
 dd status="none" if=/dev/zero of="${td}/large" bs="1M" count="$((1024/2))"
 df -h "${TMPDIR}"
 
-echo "Filling up TMPDIR, resulting in a 'No space left on device' error"
+echo "Creating an even larger file, resulting in a 'No space left on device' error:"
 dd status="none" if=/dev/zero of="${td}/huge" bs="1M" count="$((1 * 1024))"
 df -h "${TMPDIR}"
 
+echo "* EXAMPLE: Size-limited TMPDIR folder example ... DONE"
 
+
+## -------------------------------------------------------------------------
+## Script epilogue
+## -------------------------------------------------------------------------
 if [[ -n "$JOB_ID" ]]; then
-    echo "--- Job summary -------------------------------------------------"
+    echo
+    echo "=============================================================="
+    echo " End of Job Summary"
     qstat -j "$JOB_ID"
 fi    
